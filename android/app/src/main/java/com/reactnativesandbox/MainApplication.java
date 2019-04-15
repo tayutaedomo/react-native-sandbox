da0857b409e368f35748917fd6916f1e4aaa0341
tayutaedomo <tayutaedomo@gmail.com>
2019-04-15 22:12:30 +0900

    Add react-native-config amd execute link

--- android/app/src/main/java/com/reactnativesandbox/MainApplication.java
+++ android/app/src/main/java/com/reactnativesandbox/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativesandbox;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import io.sentry.RNSentryPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new RNSentryPackage(),
             new AsyncStoragePackage(),
             new PickerPackage(),
